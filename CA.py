c1035cec31ba1687d1b8c3eb170d1310c202b63b
febin123 <febsep96@gmail.com>
2022-11-25 17:44:15 +0530

    tested & formated the whole code& C:/Python310/python.exe c:/Users/febse/OneDrive/Desktop/PythonCA-1/CA.py

--- CA.py
+++ CA.py
@@ -1,34 +1,47 @@
 class Employee:
     #constructor
     def __init__(self,name,weeklyhours,rate,overtimeRate,weeklytaxcredit):
+
+         #checks the overtimeRate cannot be less than 0 
+        if(overtimeRate<0):
+            raise ValueError('Please enter correct input;It cannot be less than 0') 
+
+
         self.name=name
         self.weeklyhours=weeklyhours
         self.rate=rate
         self.overtimeRate=overtimeRate
         self.weeklytaxcredit=weeklytaxcredit
 
-        #checks the overtimeRate cannot be less than 0 
-        if(overtimeRate<0):
-            raise ValueError('It cannot be less than 0')
+       
 
     #Created a method computeWeeklyPay (self,hours)
     def computeWeeklyPay(self,hours):
         grossPay=(self.weeklyhours*self.rate)+(self.overtimeRate*hours)
-        print(grossPay)
-
-        #checks the grosspay cannot be less than 0
+        
+          #checks the grosspay cannot be less than 0
         if(grossPay<0):
             raise ValueError('Value is negative; Please enter correct input')
+
+        print(grossPay)
+
+      
 
 
     #craeted a method computeTax(self,grossPay) 
     def computeTax(self,grossPay):
         computeTax=(0.4*grossPay)-self.weeklytaxcredit
-        print(computeTax)
 
-        #checks the tax cannot be less than 0
+         #checks the tax cannot be less than 0
         if(computeTax<0):
             raise ValueError('Value is negative; Please enter correct input')
+
+        print(computeTax)
+
+       
+
+
+
 #creating objects of the class
 e1=Employee('febin',35,11,15,70)
 
